27e4c35a65e381d1fae6379adb80ed8cf80387ee
Sazal Ahmed <72685673+sazalait@users.noreply.github.com>
2023-08-13 16:40:37 +0600

    Update index.php
    test

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-echo "this is a test for websrv-5";
-echo "hello this a in the server";
+echo "this is a test for email";
+echo "hello this a in the server email";
 echo "hello mm-websrv-05";
 echo "<br>";
 echo "CI/CD pipleline mm-websrv-05"
