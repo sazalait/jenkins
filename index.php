907d8b3f212a42d32f33632031717dd64d6a8cd5
Sazal Ahmed <72685673+sazalait@users.noreply.github.com>
2023-08-13 17:19:29 +0600

    Update index.php
    Testing

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-echo "this is a test for email";
-echo "hello this a in the server email";
-echo "hello mm-websrv-05";
+echo "this is a test Email from jenkins";
+echo "hello this a in the Jenkins email";
+echo "Hello mm-websrv-05";
 echo "<br>";
 echo "CI/CD pipleline mm-websrv-05"
 ?>
