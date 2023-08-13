3cc2c11d6b9e748f7c567795c9bc95217855f232
Sazal Ahmed <72685673+sazalait@users.noreply.github.com>
2023-08-13 12:45:13 +0600

    Update index.php
    update

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-echo "this is a test for mm-websrv-05";
-echo "hello this a project mm-websrv-05";
+echo "this is a test";
+echo "hello this a ci/cd project";
 echo "hello mm-websrv-05";
 echo "<br>";
 echo "CI/CD pipleline mm-websrv-05"
